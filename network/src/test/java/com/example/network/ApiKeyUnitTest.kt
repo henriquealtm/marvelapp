26fd438e9a9567209b71143c44597936baa17f96
Henrique Altmayer <henrique_altm@hotmail.com>
2022-06-01 14:26:53 -0300

    Adding unit tests for the ApiKey object.

---
+++ network/src/test/java/com/example/network/ApiKeyUnitTest.kt
@@ -0,0 +1,17 @@
+package com.example.network
+
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ApiKeyUnitTest {
+
+    @Test
+    fun `GIVEN the initial state WHEN calling getHash() THEN return a valid value`() {
+        val apiKeyObject = mockk<ApiKey>()
+        every { apiKeyObject.getTs() } returns "1654041635344"
+        assertEquals("ec8b00c8ed4039f02a6263b9dfe9ef80", ApiKey.getHash(apiKeyObject.getTs()))
+    }
+
+}
